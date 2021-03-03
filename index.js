3e3a6a7a1d417859b1ffb14035dbeccb6e923c08
Wesley <wes.beck21@outlook.com>
2021-03-02 17:03:32 -0800

    commit tree a2368bd99385bbb7c9473d48186c089993c99f47

--- index.js
+++ index.js
@@ -1,4 +1,74 @@
 /* Your Code Here */
+let createEmployeeRecord = function(row){
+    return {
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+let createEmployeeRecords = function(arrayOfEmpData){
+    return arrayOfEmpData.map(function(d){
+        return createEmployeeRecord(d)
+    })
+}
+
+let createTimeInEvent = function(dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+    return this
+}
+
+let createTimeOutEvent = function(dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return this
+}
+
+let hoursWorkedOnDate = function(soughtDate){
+    let inEvent = this.timeInEvents.find(function(e){
+        return e.date === soughtDate
+    })
+
+    let outEvent = this.timeOutEvents.find(function(e){
+        return e.date === soughtDate
+    })
+
+    return (outEvent.hour - inEvent.hour) / 100
+}
+
+
+let wagesEarnedOnDate = function(soughtDate){
+    let rawWage = (hoursWorkedOnDate.call(this, soughtDate) 
+    * this.payPerHour)
+    return parseFloat(rawWage.toString())
+}
+
+let findEmployeeByFirstName = function(src, firstName){
+    return src.find(function(rec){
+        return rec.firstName === firstName
+    })
+}
+
+let calculatePayroll = function(EmpPay){
+    return EmpPay.reduce(function(memo, rec){
+        return memo + allWagesFor.call(rec)
+    }, 0)
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
